273521a4c72e45545da71cadabed4c42ff925732
Arslan Tsoy <tsoy.ar@rahmetapp.kz>
2019-11-19 22:12:15 +0600

    new screen

--- app/src/main/java/com/example/rahmetbusiness/MainActivity.java
+++ app/src/main/java/com/example/rahmetbusiness/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.rahmetbusiness;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogIn;
+    private EditText etSignupLogin;
+
+    private final String LOGIN = "asasfs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+
+        LOGIN = "dasd";
+
+        btnLogIn = findViewById(R.id.log_in_btn);
+        etSignupLogin = findViewById(R.id.signup_login);
+
+        btnLogIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Log.i("myOnClick", "click");
+                String textInET = etSignupLogin.getText().toString();
+                Toast.makeText(MainActivity.this, textInET, Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(MainActivity.this, Main3Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
